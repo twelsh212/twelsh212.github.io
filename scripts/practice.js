8eb56558c4cbea7ad7161afcb13900536e6c7d2e
unknown <twelsh@S21385.sogeti.com>
2015-07-07 10:05:55 -0400

    changed flow of page by adding more div tags and clearing up some css issues

--- scripts/practice.js
+++ scripts/practice.js
@@ -3,20 +3,20 @@ $(document).ready(function(){
     $("#art1btn").click(function(){
         if ($("#art1").css("display") == "none") {
             $("#art1").show(1000);
-            $(this).text("Collapse Story");
+            $("#art1btn").text("Collapse Story");
         } else {
             $("#art1").hide(1000);
-            $(this).text("Expand Story");
+            $("#art1btn").text("Expand Story");
         }
     });
     $("#art2btn").click(function() {
         if ($("#art2").css("display") == "none") {
             $("#art2").show(1000);
-            $(this).text("Collapse Story");
+            $("#art2btn").text("Collapse Story");
         } else {
             $("#art2").hide(1000);
-            $(this).text("Expand Story");
+            $("#art2btn").text("Expand Story");
         }
-        
     });
+    
 });
