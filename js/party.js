03a308f948d78d36b2079de79767aa24a828c421
twelsh212 <twelsh@bgsu.edu>
2015-07-16 13:53:58 -0400

    added stupid shutdown threat, and actually included favicon.

--- js/party.js
+++ js/party.js
@@ -9,6 +9,29 @@ $("#menu-toggle").click(function(e) {
     $("#sidebar-wrapper").toggleClass("active");
 });
 
+$(".hiddenEle").hide(0);
+$(".afterCntDown").hide(0);
+$(".shutdownTimer").click(function(e){
+   // Function to update counters on all elements with class counter
+   $(".hiddenEle").show(0);
+   var doUpdate = function() {
+      $('.target').each(function() {
+         var count = parseInt($(this).html());
+         if (count !== 0) {
+            $(this).html(count - 1);
+         }
+         if (count === 0) {
+            $(".afterCntDown").show(0);
+         }
+      });
+   };
+   setInterval(doUpdate, 1000);
+});
+
+
+  // Schedule the update to happen once every second
+
+
 // Scrolls to the selected menu item on the page
 $(function() {
     $('a[href*=#]:not([href=#])').click(function() {
@@ -24,5 +47,4 @@ $(function() {
             }
         }
     });
-
 });
