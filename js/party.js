d46d2540817a61374d1b6df01077477c624d94dc
twelsh212 <twelsh@bgsu.edu>
2015-07-16 13:14:36 -0400

    embedded map and removed modals

--- js/party.js
+++ js/party.js
@@ -9,23 +9,6 @@ $("#menu-toggle").click(function(e) {
     $("#sidebar-wrapper").toggleClass("active");
 });
 
-$(function() {
-    $( "#dialog" ).dialog({
-      autoOpen: false,
-      show: {
-        effect: "blind",
-        duration: 1000
-      },
-      hide: {
-        effect: "explode",
-        duration: 1000
-      }
-    });
-    $( ".opener" ).click(function() {
-      $( "#dialog" ).dialog( "open" );
-    });
- });
-
 // Scrolls to the selected menu item on the page
 $(function() {
     $('a[href*=#]:not([href=#])').click(function() {
