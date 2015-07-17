c698e724ea9786383354ecb5495d6d1d5eeab3c9
twelsh212 <twelsh@bgsu.edu>
2015-07-17 15:15:41 -0400

    added pictures, removed href from them, removed button from section

--- js/party.js
+++ js/party.js
@@ -28,17 +28,6 @@ $(".shutdownTimer").click(function(e){
    setInterval(doUpdate, 1000);
 });
 
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-ga('create', 'UA-65242112-1', 'auto');
-ga('send', 'pageview');
-
-  // Schedule the update to happen once every second
-
-
 // Scrolls to the selected menu item on the page
 $(function() {
     $('a[href*=#]:not([href=#])').click(function() {
